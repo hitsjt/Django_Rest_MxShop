f4ab18ed440a13e14a4238cb3abae2570bcf35e9
Jiangtao Sun <jiangtaosun@microsoft.com>
2022-03-14 11:14:03 +0800

    drf的modelserailizer实现商品列表功能--外键

--- apps/goods/serializers.py
+++ apps/goods/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
-from .models import Goods
+from .models import Goods,GoodsCategory
+
+class CategorySerializer(ModelSerializer):
+    class Meta:
+        model = GoodsCategory
+        fields = '__all__'
 
 class GoodsSerializer(ModelSerializer):
+    category = CategorySerializer()
     class Meta:
         model = Goods
         #fields = ('name','click_num','market_price','add_time')
