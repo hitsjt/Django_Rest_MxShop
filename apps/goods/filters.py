4bc9bfd742a10c3a3b2b6f16c2a4620416e607bf
Jiangtao Sun <jiangtaosun@microsoft.com>
2022-03-14 14:39:23 +0800

    filter模糊匹配

--- apps/goods/filters.py
+++ apps/goods/filters.py
@@ -4,7 +4,8 @@ from .models import Goods
 class GoodsFilter(filters.FilterSet):
     min_price = filters.NumberFilter(field_name='shop_price',lookup_expr='gte')
     max_price = filters.NumberFilter(field_name='shop_price',lookup_expr='lte')
+    name = filters.CharFilter(field_name='name',lookup_expr='icontains')
 
     class Meta:
         model = Goods
-        fields = ['min_price','max_price']
+        fields = ['min_price','max_price','name']
