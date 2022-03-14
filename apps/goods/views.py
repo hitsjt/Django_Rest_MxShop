11372a64318231f27cbedc190d2fab80d4ca7dfb
Jiangtao Sun <jiangtaosun@microsoft.com>
2022-03-14 14:43:54 +0800

    drf search filter

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from django.http import Http404
 from rest_framework.response import Response
-from rest_framework import status,generics,mixins,viewsets
+from rest_framework import status,generics,mixins,viewsets,filters
 
 from .models import Goods
 from .serializers import GoodsSerializer
@@ -24,6 +24,7 @@ class GoodsListViewSet(mixins.ListModelMixin,viewsets.GenericViewSet):
     queryset = Goods.objects.all()
     serializer_class = GoodsSerializer
     pagination_class = GoodsPagination
-    filter_backends = (DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend,filters.SearchFilter)
     filter_class = GoodsFilter
+    search_fields = ('name','goods_brief','goods_desc')
 
