e79afe1c5606328040479139774d6c7b6e0ee232
Jiangtao Sun <jiangtaosun@microsoft.com>
2022-03-14 11:22:20 +0800

    GenericView实现商品列表功能

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -1,26 +1,20 @@
 from django.shortcuts import render
 
 from django.http import Http404
-from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status,generics,mixins
 
 from .models import Goods
 from .serializers import GoodsSerializer
 
 # Create your views here.
-class GoodsListView(APIView):
+class GoodsListView(mixins.ListModelMixin,generics.GenericAPIView):
     """
     List all snippets, or create a new snippet.
     """
-    def get(self, request, format=None):
-        goods = Goods.objects.all()[:10]
-        goods_serializer = GoodsSerializer(goods, many=True)
-        return Response(goods_serializer.data)
+    queryset = Goods.objects.all()[:10]
+    serializer_class = GoodsSerializer
 
-    def post(self, request, format=None):
-        serializer = GoodsSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get(self,request,*args,**kwargs):
+        return self.list(request,*args,**kwargs)
+
